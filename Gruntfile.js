4804d1fe9907a1c86feb1434ea6c90ea5695d322
“myname” <myname@server>
2017-10-02 22:01:42 +0800

    add copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,22 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        copy: {
+            out: {
+                files: [{
+                    src: 'out/dilu.js',
+                    dest: 'example/dilu.js'
+                }]
+            }
+        },
         shell: {
             src: {
                 command: `tsc -p src`
             }
         }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-shell');
     grunt.registerTask('build', ['shell']);
-    
+
 }
