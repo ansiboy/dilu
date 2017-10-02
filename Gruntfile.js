eaf899f62d7634fde19cf1f4375b1a684f0c2de0
maishu <ansiboy@163.com>
2017-10-02 22:03:17 +0800

    add connect

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,30 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        connect: {
+            examples: {
+                options: {
+                    // 服务器端口号
+                    port: 8041,
+                    // 服务器地址(可以使用主机名localhost，也能使用IP)
+                    hostname: 'localhost',
+                    keepalive: true,
+                    // livereload: 35729,
+                    // 物理路径(默认为. 即根目录) 注：使用'.'或'..'为路径的时，可能会返回403 Forbidden. 此时将该值改为相对路径 如：/grunt/reloard。
+                    base: 'examples',
+                    open: true//,hashchange
+                }
+            }
+        },
         shell: {
             src: {
                 command: `tsc -p src`
             }
         }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-connect')
     grunt.loadNpmTasks('grunt-shell');
     grunt.registerTask('build', ['shell']);
-    
+    grunt.registerTask('default', ['build', 'copy', 'connect'])
+
 }
