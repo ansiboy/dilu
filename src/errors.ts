ba8be3afa31293d0113f61515ab02cd4d1570a8f
maishu <ansiboy@163.com>
2017-10-05 19:17:08 +0800

    重构

--- src/errors.ts
+++ src/errors.ts
@@ -4,9 +4,6 @@ namespace dilu {
             let msg = `Parameter ${parameterName} can not be null or empty.`
             return new Error(msg);
         },
-        ruleNotExists(name: string) {
-            return new Error(`Rule named ${name} is not exists.`);
-        },
         elementValidateRuleNotSet(element: HTMLInputElement) {
             let msg = `元素'${element.name}'没有设置验证规则`;
             return new Error(msg);
