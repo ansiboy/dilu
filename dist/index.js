d000138081113731a604cc56ba4e7a3339d09ea7
mai.shu <ansiboy@163.com>
2020-04-20 12:19:56 +0800

    generate code

--- dist/index.js
+++ dist/index.js
@@ -146,10 +146,11 @@ exports.errors = {
 "use strict";
 
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -431,8 +432,8 @@ class FormValidator {
         return element.name;
     }
 }
-FormValidator.errorClassName = 'validationMessage';
 exports.FormValidator = FormValidator;
+FormValidator.errorClassName = 'validationMessage';
 // }
 
 
