07f2e3339898596f11872c4ce490cebf0d302c4c
mai.shu <ansiboy@163.com>
2021-03-02 16:04:51 +0800

    该样式

--- src/style.ts
+++ src/style.ts
@@ -4,7 +4,7 @@ let elementId = "maishu-dilu-style";
 if (!document.getElementById(elementId) && document.head != null) {
     let element = document.createElement('style');
     element.type = 'text/css';
-    element.id = "maishu-jueying-core-style";
+    element.id = elementId;
     document.head.appendChild(element);
     element.innerHTML = `
     .${FormValidator.errorClassName} {
