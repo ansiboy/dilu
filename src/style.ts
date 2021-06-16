1a25e57a299f039c2afcf8fa69741721211a3eee
mai.shu <ansiboy@163.com>
2021-06-16 11:24:17 +0800

    对 document 为空进行判断

--- src/style.ts
+++ src/style.ts
@@ -1,16 +1,18 @@
 import { FormValidator } from "./formValidator";
 
 let elementId = "maishu-dilu-style";
-if (!document.getElementById(elementId) && document.head != null) {
-    let element = document.createElement('style');
-    element.type = 'text/css';
-    element.id = elementId;
-    document.head.appendChild(element);
-    element.innerHTML = `
-    .${FormValidator.errorClassName} {
-        color: red;
-        font-weight: bold;
+if (typeof document != "undefined") {
+    if (!document.getElementById(elementId) && document.head != null) {
+        let element = document.createElement('style');
+        element.type = 'text/css';
+        element.id = elementId;
+        document.head.appendChild(element);
+        element.innerHTML = `
+        .${FormValidator.errorClassName} {
+            color: red;
+            font-weight: bold;
+        }
+        `
     }
-    `
 }
 
